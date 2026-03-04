ec5944b2c681c80d634c812662dacadd47ee2038
Florence Etienne <florence.etienne@mail.citytech.cuny.edu>
2026-03-04 07:30:39 -0500

    create project and plan idea

---
+++ MIDTERM_FlorenEtienne/MIDTERM_FlorenEtienne.ino
@@ -0,0 +1,26 @@
+/*
+Blinking Light Show System
+
+Using 1 Button to toggle on and off the System
+
+Using 1 Green LED for indicating "Open"
+Using 1 Red LED for indicating "Close"
+
+Using 3 White LEDs for the Mid-term's patterns
+
+- LED 1 Pulse
+- LED 2 Jolt
+- LED 3 Blink
+
+Using 1 Pot to change the duration of a pattern (maybe?) or how much they blink?
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
